d100f62de4b23ce90bd71044e38f3c10427078e5
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-19 22:12:22 +0300

    Update test2.php

--- test2.php
+++ test2.php
@@ -2,12 +2,9 @@ add_action( 'rest_api_init', function() {
 
 	register_rest_route( 'iq/v1', 'redirect_add', array(
 		'methods'             => 'POST',            
-		'callback'            => 'function_name',  
-		'permission_callback' => 'function_name', 
 		'args' => array(
 			'link' => array(
-				'default'           => null,           // значение параметра по умолчанию
-				'required'          => null,           // является ли параметр обязательным. Может быть только true
+				'required'          => null,    
 			),
 		),
 	) );
