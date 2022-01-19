7ccd48c0754ae2678fb949ca1c577aff7be317cd
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-19 19:43:35 +0300

    Update test.php

--- test.php
+++ test.php
@@ -4,11 +4,11 @@ function create_table() {
 
 	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
-	$table_name = $wpdb->ger_blog_prefix() . 'test_table';
+	$table_name = $wpdb->get_blog_prefix() . 'test_table';
 	$charset_collate = $wpdb->get_charset_collate();
 
 	$sql = "CREATE TABLE {$test} (
-		id string(8) unsigned NOT NULL auto_increment,
+		id varchar(255) unsigned NOT NULL auto_increment,
 		link varchar(255) NOT NULL default'',
 		PRIMARY KEY (id),
 		KEY link (link)
