5402b71fb07b3b9162e7c06c60fb1f538094fc07
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-19 19:34:32 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,26 +1,20 @@
+<?
+function create_table() {
+	global $wpdb;
 
-				<?php
-				while ( have_posts() ) {
-					the_post();
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
-					get_template_part( 'loop-templates/content', 'single' );
-                     echo ("Площадь:  ");					
-                     the_field('площадь');
-					 echo ("<br>     Стоимость:  ");					
-                     the_field('стоимость');
-                     echo ("<br>     Адрес:  ");					
-                     the_field('адрес');
-                     echo ("<br>     Жилая площадь:  ");					
-                     the_field('жилая_площадь');
-                     echo ("<br>     Этаж:  ");					
-                     the_field('этаж');
-					understrap_post_nav();
+	$table_name = $wpdb->ger_blog_prefix() . 'test_table';
+	$charset_collate = $wpdb->get_charset_collate();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				}
-				?>
+	$sql = "CREATE TABLE {$test} (
+		id string(8) unsigned NOT NULL auto_increment,
+		link varchar(255) NOT NULL default'',
+		PRIMARY KEY (id),
+		KEY link (link)
+	    )
+	{$charset_collate};";
 
-			</main><!-- #main -->
+	dbDelta($sql);
+}
+?>
