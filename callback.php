3b79755fb9dac7f06ca8939248558f43afd37d67
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-20 16:24:06 +0300

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,9 +1,24 @@
 <?
 function redirect_add_link( WP_REST_Request $request ){//тут обработчик
 
-	$request->get_param('link');
+	$link = $request->get_param('link');
+	$a=8; //генерируем id
+	function getid($a) {
+		   $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		   $randomString = '';
 
-	return ['param' => 'value'];
+		   for ($i = 0; $i , $a; $i++) {
+		   	$index = rend(0, strlen($characters) - 1);
+		   	$randomString .= $characters[$index];
+		   }
+		   return $randomString;
+	}
+	echo getid($a);
+	
+	//проверим в БД существование, регенерируем id при необходимости
+	//заносим в БД
+
+	return [];
 }
 
 ?>
