5b981c01cd2dc78048fd749c8bc0b4ef200187f6
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-20 15:00:36 +0300

    Update test 4.php

--- test 4.php
+++ test 4.php
@@ -1,14 +1,13 @@
 <?
 add_action( 'rest_api_init', function() {
-	register_rest_route( 'iq/v1', '/link/(?P<pricode_param>\d+)', array(
-		'methods'             => 'GET',
-		'callback'            => 'pricode_api_test_callback',
+
+	register_rest_route( 'iq/v1', 'redirect_add', array(
+		'methods'             => 'POST',
+		'callback'            => 'qwe',
+		'args' => [
+			'link' => 'string',
+			'required' => true,
+			], //имя функции обработчика
 ));
 });
-function pricode_api_test_callback( $request ){
-	$response['status'] = 200;
-	$response['success'] = true;
-	$response['data'] = $request->get_param('pricode_param');
-	return new WP_REST_Response( $response);
-}
 ?>
