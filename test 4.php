19a3457d1665c145866d70c111afc310d7f632dc
Dudarev31 <44091792+Dudarev3101@users.noreply.github.com>
2022-01-20 15:19:04 +0300

    Update test 4.php

--- test 4.php
+++ test 4.php
@@ -3,23 +3,21 @@ add_action( 'rest_api_init', function() {
 
 	register_rest_route( 'iq/v1', 'redirect_add', array(
 		'methods'             => 'POST',
-		'callback'            => 'add', //имя функции обработчика
+		'callback'            => 'redirect_add_link', //имя функции обработчика
 		'args' => [
 			'link' => ['type' =>'string',
 			'required' => true,
 			],
 		]
 ));
-});
 	register_rest_route( 'iq/v1', 'redirect_list', array(
 		'methods'             => 'POST',
-		'callback'            => 'list', //имя функции обработчика
+		'callback'            => 'redirect_list_show', //имя функции обработчика
 		'args' => []
 ));
-});
 	register_rest_route( 'iq/v1', 'redirect_delete', array(
 		'methods'             => 'POST',
-		'callback'            => 'delete', //имя функции обработчика
+		'callback'            => 'redirect_delete_link', //имя функции обработчика
 		'args' => [
 			'id' => ['type' =>'string',
 			'required' => true,
